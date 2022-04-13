66a19b1e80cc7f4f34a6a861a428e51424db9845
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-04-13 11:57:30 +0000

    Add assertions to function

--- typescript.js
+++ typescript.js
@@ -3,3 +3,45 @@ var sum = function (a, b) {
 };
 var answer = sum(4, 5);
 console.log(answer);
+var isCool = true;
+// number
+var age = 56;
+//string
+var eyeColor = 'brown';
+var favoriteQuote = "I'm not old, I'm only ".concat(age, " years old");
+//Array 
+var pets = ['cats', 'dogs', 'pigs'];
+var pet2 = [2, 'dogs', 'pigs'];
+//Object
+var wizard = {
+    name: 'John'
+};
+// null and undefined
+var meh = undefined;
+var noo = null;
+//Uncommon Types in TypeScript
+//1. Tuple
+var basket;
+basket = ['basketball', 5];
+//2. Enum
+var Size;
+(function (Size) {
+    Size[Size["small"] = 1] = "small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {})); //Enum are used for iteratables mostly for defining a contant values
+var sizeName = Size.small; // Its declaraion is similar to an object but differs a little bit
+// 3. void => It should have a reachable end point, It should end or return something
+var sing = function () {
+    console.log('lalallalaala');
+};
+// 4. never => It should not have a reachable end point i.e it should not end
+var error = function () {
+    throw Error('Ooops!');
+};
+var fightRobotArmy = function (robots) {
+    console.log('FIGHT!');
+};
+var fightRobotArmy2 = function (robots) {
+    console.log('FIGHT!');
+};
--- typescript.ts
+++ typescript.ts
@@ -5,3 +5,86 @@ var sum = (a: number, b: number)=> {
     var answer = sum(4,5)
     console.log(answer);
     
+let isCool: boolean = true
+
+
+// number
+let age: number = 56;
+
+//string
+
+let eyeColor: string = 'brown';
+let favoriteQuote: string = `I'm not old, I'm only ${age} years old`;
+
+//Array 
+
+let pets: string[] = ['cats', 'dogs', 'pigs']
+let pet2: any[] = [2, 'dogs', 'pigs']
+
+//Object
+
+let wizard: object = {
+    name: 'John',
+}
+
+// null and undefined
+
+let meh: undefined = undefined
+
+let noo: null = null
+
+//Uncommon Types in TypeScript
+
+//1. Tuple
+
+let basket:[string, number];
+
+basket =['basketball', 5]
+
+//2. Enum
+
+enum Size { small=1, Medium=2, Large=3 }  //Enum are used for iteratables mostly for defining a contant values
+let sizeName: number = Size.small;        // Its declaraion is similar to an object but differs a little bit
+
+// 3. void => It should have a reachable end point, It should end or return something
+
+let sing =(): void =>{
+    console.log('lalallalaala');
+}
+
+// 4. never => It should not have a reachable end point i.e it should not end
+
+let error=(): never=> {
+    throw Error('Ooops!')
+}
+
+
+// 5 Type Assertions
+interface RobotArmy{
+    count: number,
+    type: string,
+    magic: string
+}
+
+let fightRobotArmy=(robots: RobotArmy) =>{
+    console.log('FIGHT!');
+}
+
+let fightRobotArmy2=(robots: {  count: number, // Both fightRobotArmy and fightRobotArmy2 are the same
+    type: string,
+    magic: string}) =>{
+    console.log('FIGHT!');
+}
+
+
+
+interface CatArmy{
+    count: number,
+    type: string,
+    magic?: string
+}
+
+
+
+
+
